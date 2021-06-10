7fcab13a2b728d7f13a98357aa1de88ee105cb40
Fernanda Domenicali <nanda21domenicali@gmail.com>
2021-06-10 18:14:47 -0300

    button action

--- src/app/nova-transferencia.component.ts
+++ src/app/nova-transferencia.component.ts
@@ -3,8 +3,11 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-nova-transferencia',
     templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+    styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
 
+export class NovaTransferenciaComponent {
+  transferir(){
+    console.log('solicitado nova transferencia');
+  }
 }
